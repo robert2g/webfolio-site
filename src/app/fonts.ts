ccfb97cb382b3259834a358c493938fbc81d331c
robert2g <jose.gglassman@proton.me>
2024-11-01 22:27:43 -0500

    Removed TT Octosquares local fonts and use in fonts.ts file.

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,3 @@
-import localFont from "next/font/local";
 import { Ubuntu_Sans } from 'next/font/google'
 
 /* Open Sans Variable Font - through Google font API */
@@ -6,29 +5,3 @@ export const ubuntuSans = Ubuntu_Sans({
     subsets: ['latin'],
     display: 'swap',
 });
-
-/* TT OctoSquares Font Family - local font, non-variable. */
-export const octoSquares = localFont({
-    src: [
-        {
-            path: './fonts/TTOctosquaresCond-Medium.woff2',
-            weight: '500',
-        },
-        {
-            path: './fonts/TTOctosquaresCond-Regular.woff2',
-            weight: '400',
-        },
-        {
-            path: './fonts/TTOctosquaresCond-Thin.woff2',
-            weight: '300',
-        },
-        {
-            path: './fonts/TTOctosquaresCond-Light.woff2',
-            weight: '200',
-        },
-        {
-            path: './fonts/TTOctosquaresCond-XLight.woff2',
-            weight: '100',
-        },
-    ]
-})
